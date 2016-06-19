7c594b8e56d160dfb16370d966ac3da390e210c6
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2016-06-19 14:12:45 +0900

    add Strategy codes

---
+++ homework/StrategyPattern/ApplicationRunner.java
@@ -0,0 +1,13 @@
+public class ApplicationRunner
+{
+    private Application itsApplication = null;
+    public ApplicationRunner(Application app){
+        itsApplication=app;
+    }
+    public void run(){
+        itsApplication.init();
+        while(!itsApplication.done())
+            itsApplication.idle();
+        itsApplication.cleanup();
+    }
+}
