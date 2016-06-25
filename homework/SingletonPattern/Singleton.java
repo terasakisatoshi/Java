0a5bdf6835306173944ff37b841f2e1df2768a57
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2016-06-25 12:48:57 +0900

    add SIngleton-pattern

---
+++ homework/SingletonPattern/Singleton.java
@@ -0,0 +1,11 @@
+public class Singleton
+{
+    private static Singleton theInstance = null;
+    private Singleton(){};
+    public static Singleton Instance(){
+        if(theInstance==null){
+            theInstance = new Singleton();
+        }
+        return theInstance;
+    }
+}
